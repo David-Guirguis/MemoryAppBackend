b21286d276604da2f1cc8dcc79fb98e1531c397c
David-Guirguis <38105868+David-Guirguis@users.noreply.github.com>
2020-12-21 09:20:31 +0100

    Update MemoryService.java
    Added comments

--- src/main/java/com/example/memoryappbackend/rest/MemoryService.java
+++ src/main/java/com/example/memoryappbackend/rest/MemoryService.java
@@ -18,22 +18,26 @@ public class MemoryService {
     @Autowired
     private MemoryBusinessLogic memoryBusinessLogic;
 
+   //REST GET-method for getting all memories for specific user
     @GetMapping("/getUserMemories")
     @CrossOrigin(origins = "http://localhost:3000")
     public List<MemoryItem> getUserMemories(@RequestParam String googleID) {
         return memoryBusinessLogic.getUserMemories(googleID);
     }
 
+   //REST POST-method for posting a memory
     @PostMapping("/postMemory")
     public void postMemory(@RequestBody MemoryItemDTO mDTO) {
         memoryBusinessLogic.postMemory(mDTO.getGoogleID(), mDTO.getMemoryTitle(), mDTO.getMemoryBody(), mDTO.getTags());
     }
 
+   //REST POST-method for deleting a memory
     @PostMapping("/deleteMemory")
     public void deleteMemory(@RequestBody int memoryID) {
         memoryBusinessLogic.deleteMemory(memoryID);
     }
 
+   //REST POST-method for editing a memory
     @PostMapping("/editMemory")
     public void editMemory(@RequestBody EditMemoryDTO mDTO) {
         memoryBusinessLogic.editMemory(mDTO.getGoogleID(), mDTO.getMemoryID(), mDTO.getMemoryTitle(), mDTO.getMemoryBody(), mDTO.getTags());
